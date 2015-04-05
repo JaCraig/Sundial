97ed1e36c66c070e1970a503225ab92bd4d7aa38
James Craig <admin@gutgames.com>
2015-04-04 22:43:18 -0400

    Set up interfaces, etc. Still need to finish the config/program portion.

---
+++ Sundial.Core/Result.cs
@@ -0,0 +1,64 @@
+﻿/*
+Copyright (c) 2015 <a href="http://www.gutgames.com">James Craig</a>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.*/
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sundial.Core
+{
+    /// <summary>
+    /// Data results
+    /// </summary>
+    public class Result
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Result"/> class.
+        /// </summary>
+        /// <param name="times">The times.</param>
+        /// <param name="name">The name.</param>
+        public Result(IEnumerable<long> times, string name)
+        {
+            this.Times = times ?? new List<long>();
+            this.Name = string.IsNullOrEmpty(name) ? "" : name;
+        }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets the times.
+        /// </summary>
+        /// <value>The times.</value>
+        public IEnumerable<long> Times { get; private set; }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <returns>A <see cref="System.String"/> that represents this instance.</returns>
+        public override string ToString()
+        {
+            return Name + ": " + Times.Average();
+        }
+    }
+}
---
+++ Sundial/Program.cs
@@ -0,0 +1,40 @@
+﻿using Sundial.Core;
+using Sundial.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using Utilities.DataTypes;
+using Utilities.Profiler.Manager;
+using Utilities.Random;
+
+namespace Sundial
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            System.Random TempRand = new System.Random();
+            using (var Bootstrapper = Utilities.IoC.Manager.Bootstrapper)
+            {
+                IEnumerable<IDataFormatter> Formatters = Bootstrapper.ResolveAll<IDataFormatter>();
+                IEnumerable<ITimedTask> TimedTasks = TempRand.Shuffle(Bootstrapper.ResolveAll<ITimedTask>());
+
+                using (var Profiler = Manager.StartProfiling())
+                {
+                    foreach (ITimedTask Task in TimedTasks)
+                    {
+                        for (int x = 0; x < NumberIterations; ++x)
+                        {
+                            using (var Timer = Manager.Profile(Task.Name))
+                            {
+                                Task.Run();
+                            }
+                        }
+                    }
+                }
+                var Result = Manager.StopProfiling(false);
+                IEnumerable<Result> Results = Result.Children.ForEach(x => new Result(x.Value.Times, x.Key));
+                Formatters.ForEach(x => x.Format(Results));
+            }
+        }
+    }
+}
