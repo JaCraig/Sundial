e6755136846e27858255bb9dc4b9776222618048
James Craig <admin@gutgames.com>
2015-05-05 23:22:20 -0400

    Modified production batch script. Still need to automatically add a new release to github and then upload to chocolatey a new version.

--- CopyForProduction.bat
+++ CopyForProduction.bat
@@ -8,3 +8,12 @@ cd Package
 del "*.dll"
 del "*.exe"
 del "*.config"
+cd ..
+
+echo "Commit, add git release tag, and push to github. Then add sundial.zip to the release... Not sure on that part..."
+
+cd chocolatey
+echo "Modify version info of nuspec file"
+cpack
+FOR %%A IN (*.nupkg) DO choco push %%A
+del "*.nupkg"
--- chocolatey/tools/chocolateyuninstall.ps1
+++ chocolatey/tools/chocolateyuninstall.ps1
@@ -11,12 +11,4 @@ $installerType = 'EXE'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
-#$osBitness = Get-ProcessorBits
- 
-#if ($osBitness -eq 64) {
-#  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
-#} else {
-#  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
-#} 
-#	
-#Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+Uninstall-ChocolateyZipPackage -PackageName $packageName
